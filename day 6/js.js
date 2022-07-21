f456f66490e593942e2c6b447d6f7b9f78cf4cd5
diken.dev <diken.dev@gmail.com>
2022-07-20 23:46:18 -0300

    Added day 6

--- day 6/js.js
+++ day 6/js.js
@@ -1,68 +1,82 @@
-const countries = [
-  'Brazil',
-  'Venezuela',
-  'Argentina',
-  'Peru',
-  'Alemanha',
-  'USA',
-  'Bolivia',
-  'Chile'
-]
-
-const webTechs = [
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'React',
-  'Angular',
-  'Java',
-  'Python',
-  'Node',
-  'Redux'
-]
-
-const frontStack = ['HTML', 'CSS', 'JavaScript', 'React']
-
-
-//for (i = 0; i <= 10; i++) {
-//  console.log(i)
-//}
-
-//for (i = 10; i >= 0; i--) {
-//  console.log(i)
-//}
-
-
-/*
-Write a loop that makes the following pattern using console.log():
-
-    #
-    ##
-    ###
-    ####
-    #####
-    ######
-    #######
-
-
-//for (let i = 1; i <=7; i++){
-//  console.log('# '.repeat(i));
-//}
-console.log('1 i^2 i^3') 
-for (let i = 0; i <= 10; i++){
-  console.log(`${i}  ${i*i}  ${i*i*i}`) 
+// Exercicios dia 6
+// 1.
+function one1 (one1){
+  for(let i = 0; i <= 10; i++){
+    console.log(i);
+  }
 }
-*/
 
-for (let i = 0; i <=100; i++){
-  if (i === 0){
+function one2 (one2){
+  let i = 0;
+  while (i <= 10){
     console.log(i);
-  
-  } if (i % 2 === 0){
+    i++
+  }
+}
+
+function one3 (one3){
+  let i = 0;
+  do {
     console.log(i);
+    i++
+  } while (i <= 10)
+}
+
+// 2.
+function two1 (two1){
+  for(let i = 10; i >= 0; i--){
+    console.log(i);
+  }
+}
+
+function two2 (two2){
+  let i = 10
+  while ( i >= 0 ){
+    console.log(i);
+    i--
+  }
+}
+
+function two3 (two3){
+  let i = 10;
+  do {
+    console.log(i);
+    i--
+  } while (i >= 0)
+}
+
+// 3.
+function three(three){
+  let n = 10;
+  for( i = 0; i <= n; i++){
+   console.log(i);
+  }
+}
+
+
+// 4.
+function four(four){
+  for(var i=1; i<=8; i++){
+    console.log("* ".repeat(i));
+  }
+}
+
+// 5.
+function five(five){
+  let i = 0;
+  do{
+    console.log(`${i} * ${i} = ${i * i}` );
+    i++
+  } while (i <= 10)
+}
+
+// 6.
+function six(six){
+  let i = 0;
+  console.log('i i^2 i^3');
+  while (i <= 10){
+    console.log(`${i}  ${i**2}  ${i**3}`);
+    i++
   } 
-
-  else {
-    continue
-  }  
 }
+six()
