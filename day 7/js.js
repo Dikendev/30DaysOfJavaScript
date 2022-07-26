c0c28e098fa36c2c1e6690d883afb02087aebe0b
diken.dev <diken.dev@gmail.com>
2022-07-25 22:38:12 -0300

    added exercise n 8

--- day 7/js.js
+++ day 7/js.js
@@ -33,6 +33,41 @@ function areaOfRectangle(length, width) {
 }
 // console.log('area of Rectangle: ', areaOfRectangle(23,12),"cm");
 
-//5.
+// 5.
 // A perimeter of a rectangle is calculated as follows: perimeter= 2x(length + width). Write a function which calculates perimeterOfRectangle.
+
+function perimeterOfRectangle(length, width) {
+    perimeter = 2 * (length * width)
+    return perimeter;
+}
+// console.log('Perimeter of Rectangle: ', perimeterOfRectangle(3,3));
+
+// 6.
+// A volume of a rectangular prism is calculated as follows: volume = length x width x height. Write a function which calculates volumeOfRectPrism.
+
+function volumeOfRectPrism(length, width, height) {
+    volume = (length * width * height);
+    return volume;
+}
+// console.log('Volume of rectangular prism: ', volumeOfRectPrism(10,10,23));
+
+// 7.
+// Area of a circle is calculated as follows: area = π x r x r. Write a function which calculates areaOfCircle
+
+function areaOfCircle(r) {
+    area = Math.PI * r * r;
+    return area;
+}
+// console.log('Area of Circle: ', areaOfCircle(2));
+
+// 8.
+//Circumference of a circle is calculated as follows: circumference = 2πr. Write a function which calculates circumOfCircle
+
+function circumOfCircle(r) {
+    circumference = (2 * Math.PI) * r;
+    return circumference;
+}
+// console.log('Circumference of circle is: ', circumOfCircle(23));
+
+
 
