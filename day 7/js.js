e2454ccb7267e28bd6803502c1b3ed4ea29b077c
diken.dev <diken.dev@gmail.com>
2022-07-25 22:14:56 -0300

    added exercise n 4

--- day 7/js.js
+++ day 7/js.js
@@ -10,15 +10,29 @@ function fullName() {
 // Declare a function fullName and now it takes firstName, lastName as a parameter and it returns your full - name.
 
 function fullName2(firstName, lastName) {
-    fullName = `${firstName} ${lastName}`
+    fullName = `${firstName} ${lastName}`;
     return fullName; 
 }
-console.log(fullName2('Diego Kennedy', 'Guimaraes Barros'));
+// console.log(fullName2('Diego Kennedy', 'Guimaraes Barros'));
  
 // 3.
 // Declare a function addNumbers and it takes two two parameters and it returns sum.
 
 function addNumbers(a, b) {
     sum  = a + b;
-    return sum
+    return sum;
 }
+// console.log(addNumbers(2,2));
+
+// 4.
+// An area of a rectangle is calculated as follows: area = length x width. Write a function which calculates areaOfRectangle.
+
+function areaOfRectangle(length, width) {
+    area = (length * width);
+    return area;
+}
+// console.log('area of Rectangle: ', areaOfRectangle(23,12),"cm");
+
+//5.
+// A perimeter of a rectangle is calculated as follows: perimeter= 2x(length + width). Write a function which calculates perimeterOfRectangle.
+
