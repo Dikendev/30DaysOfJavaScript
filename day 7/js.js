fe901f84056515681237a8224d49292ed251bb2b
diken.dev <diken.dev@gmail.com>
2022-07-26 01:24:49 -0300

    fixed day 7 formating

--- day 7/js.js
+++ day 7/js.js
@@ -2,7 +2,7 @@
 // Declare a function fullName and it print out your full name.
 
 function fullName() {
-    console.log('Diego Kennedy Guimaraes Barros');
+  console.log('Diego Kennedy Guimaraes Barros');
 }
 // fullName();
 
@@ -10,8 +10,8 @@ function fullName() {
 // Declare a function fullName and now it takes firstName, lastName as a parameter and it returns your full - name.
 
 function fullName2(firstName, lastName) {
-    let fullName = `${firstName} ${lastName}`;
-    return fullName; 
+  let fullName = `${firstName} ${lastName}`;
+  return fullName; 
 }
 // console.log(fullName2('Diego Kennedy', 'Guimaraes Barros'));
  
@@ -19,8 +19,8 @@ function fullName2(firstName, lastName) {
 // Declare a function addNumbers and it takes two two parameters and it returns sum.
 
 function addNumbers(a, b) {
-    let sum  = a + b;
-    return sum;
+  let sum  = a + b;
+  return sum;
 }
 // console.log(addNumbers(2,2));
 
@@ -28,8 +28,8 @@ function addNumbers(a, b) {
 // An area of a rectangle is calculated as follows: area = length x width. Write a function which calculates areaOfRectangle.
 
 function areaOfRectangle(length, width) {
-    let area = (length * width);
-    return area;
+  let area = (length * width);
+  return area;
 }
 // console.log('area of Rectangle: ', areaOfRectangle(23,12),"cm");
 
@@ -37,8 +37,8 @@ function areaOfRectangle(length, width) {
 // A perimeter of a rectangle is calculated as follows: perimeter= 2x(length + width). Write a function which calculates perimeterOfRectangle.
 
 function perimeterOfRectangle(length, width) {
-    let perimeter = 2 * (length * width)
-    return perimeter;
+  let perimeter = 2 * (length * width)
+  return perimeter;
 }
 // console.log('Perimeter of Rectangle: ', perimeterOfRectangle(3,3));
 
@@ -46,8 +46,8 @@ function perimeterOfRectangle(length, width) {
 // A volume of a rectangular prism is calculated as follows: volume = length x width x height. Write a function which calculates volumeOfRectPrism.
 
 function volumeOfRectPrism(length, width, height) {
-    let volume = (length * width * height);
-    return volume;
+  let volume = (length * width * height);
+  return volume;
 }
 // console.log('Volume of rectangular prism: ', volumeOfRectPrism(10,10,23));
 
@@ -55,8 +55,8 @@ function volumeOfRectPrism(length, width, height) {
 // Area of a circle is calculated as follows: area = π x r x r. Write a function which calculates areaOfCircle
 
 function areaOfCircle(r) {
-    let area = Math.PI * r * r;
-    return area;
+  let area = Math.PI * r * r;
+  return area;
 }
 // console.log('Area of Circle: ', areaOfCircle(2));
 
@@ -64,8 +64,8 @@ function areaOfCircle(r) {
 // Circumference of a circle is calculated as follows: circumference = 2πr. Write a function which calculates circumOfCircle
 
 function circumOfCircle(r) {
-    let circumference = (2 * Math.PI) * r;
-    return circumference;
+  let circumference = (2 * Math.PI) * r;
+  return circumference;
 }
 // console.log('Circumference of circle is: ', circumOfCircle(23));
 
@@ -73,8 +73,8 @@ function circumOfCircle(r) {
 // Density of a substance is calculated as follows:density= mass/volume. Write a function which calculates density.
 
 function densityobj(mass, volume) {
-    let density = mass / volume;
-    return density; 
+  let density = mass / volume;
+  return density; 
 }
 // console.log('density: ', densityobj(30,2));
 
@@ -82,8 +82,8 @@ function densityobj(mass, volume) {
 // Speed is calculated by dividing the total distance covered by a moving object divided by the total amount of time taken. Write a function which calculates a speed of a moving object, speed.
 
 function speedobj(distance, timetaken) {
-    let speed = distance / timetaken;
-    return speed;
+  let speed = distance / timetaken;
+  return speed;
 }
 // console.log("Speed: ", speedobj(10, 2), 'm/s');
 
@@ -103,22 +103,18 @@ const convertCelsiusToFahrenheit = (oC) => (oC * 9/5) + 32;
 // Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2. Write a function which calculates bmi. BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
 
 const BodyMassIndex = (weight, height) => {
-    let bmi = weight / (height * height);
+  let bmi = weight / (height * height);
 
-    if (bmi >= 30) {
-        console.log('Obese');
-    } else if (bmi >= 25) {
-        console.log('Overweight');
-    } else if (bmi >= 18.5) {
-        console.log('Normal');
-    } else {
-        console.log('Underweight');
-    }
-
-
-    
+  if (bmi >= 30) {
+    console.log('Obese');
+  } else if (bmi >= 25) {
+    console.log('Overweight');
+  } else if (bmi >= 18.5) {
+    console.log('Normal');
+  } else {
+    console.log('Underweight');
+  } 
 }
-
 BodyMassIndex(2220, 3)
 
 
