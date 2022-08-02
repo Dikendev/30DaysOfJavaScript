d1a96ac86028a1ad2523703e763328e9f027e3e7
diken.dev <diken.dev@gmail.com>
2022-08-01 23:40:03 -0300

    Added exercise lvl 1 - 1 to day 9

--- day 9/js.js
+++ day 9/js.js
@@ -11,3 +11,13 @@ const products = [
   { product: 'coffee', price: 10 },
   { product: 'tea', price: '' },
 ];
+
+// Explain the difference between forEach, map, filter, and reduce.
+/*
+forEach: Iterate an array elements, only with arrays, it takes a callback function with elements, index parameter and array itself.
+map: Iterate an array elements and modify the array elements, it takes a callback function with elements, index, array parameter and return a new array.
+filter: Filter out items wich full fill filtering conditions and return a new array.
+reduce: Takes a callback function, takes acumulator, current, and optional initial values as a parameter and returns a single value.
+*/
+
+// Define a callback function before you use it in forEach, map, filter or reduce.
