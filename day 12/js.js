fe259c4a83f491cf959379addbdd8f2a9979803d
diken.dev <diken.dev@gmail.com>
2022-08-10 22:56:57 -0300

    added exercise lvl 1 - 1 to day 12

--- day 12/js.js
+++ day 12/js.js
@@ -1,1 +1,15 @@
-console.log('a')
+// lvl 1 - 1.
+// Calculate the total annual income of the person from the following text. ‘He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.’
+
+const text =  'He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.';
+const pattern = /\d+/g
+const matches = text.match(pattern);
+let sum = 0 // result string array
+  
+var incomeTotal = matches.map(function (x) { 
+  return parseInt(x, 10); 
+}); // convert to number array
+
+incomeTotal.forEach(num => sum += num);
+console.log(sum);
+
