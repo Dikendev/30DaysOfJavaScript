f3126445432510d6dcb388557147e6467542a5a1
diken.dev <diken.dev@gmail.com>
2022-08-12 00:18:36 -0300

    fixed exercise lvl 1 - 1 day 12

--- day 12/js.js
+++ day 12/js.js
@@ -13,3 +13,25 @@ var incomeTotal = matches.map(function (x) {
 incomeTotal.forEach(num => sum += num);
 console.log(sum);
 
+// lvl 1 - 2.
+// The position of some particles on the horizontal x-axis -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction. Extract these numbers and find the distance between the two furthest particles.
+
+let points = ['-1', '2', '-4', '-3', '-1', '0', '4', '8'];
+let stringPoints = points.toString()
+console.log(stringPoints)
+
+let patternPoints = /^[1-9]+[0-9]*$/;
+let result = stringPoints.match(patternPoints);
+console.log(result);
+
+// console.log(points)
+
+
+// const sortePoints = points.sort(function (a,b) {return a-b});
+// console.log(sortePoints)
+// const matchesP = sortePoints.match(patternPoints)
+// console.log(matchesP)
+
+// var sortePoints = points.map(function (x) {
+//   return parseInt(x, 10);
+// });
