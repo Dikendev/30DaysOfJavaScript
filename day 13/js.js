3b00226f4175841ee767873c91590e6bc65d8404
diken.dev <diken.dev@gmail.com>
2022-08-13 20:20:33 -0300

    added exercises lvl 2 - 1,2,3 to day 13

--- day 13/js.js
+++ day 13/js.js
@@ -19,9 +19,22 @@ const country = {
 console.table(country);
 
 // lvl 1 - 3.
-// Use console.group() to group logs
+// Use console.group() to group logs.
 console.group('Countries')
 console.log(countries);
 console.groupEnd()
 
+// lvl 2 - 1.
+// 10 > 2 * 10 use console.assert().
+console.assert(10 > 2 * 10, 'Erro' );
+
+// lvl 2 - 2.
+// Write a warning message using console.warn().
+
+console.warn('Alerta, vou ser um desenvolvedor profissional em breve')
+
+// lvl 2 - 3.
+// Write an error message using console.error().
+
+console.error('Mind set poderoso para estudar com foco e objetivo');
 
