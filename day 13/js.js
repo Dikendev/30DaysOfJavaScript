ba6763b06eab7a65457474c1c5a8a55197362463
diken.dev <diken.dev@gmail.com>
2022-08-13 20:50:27 -0300

    added exercise lvl 3 to day 13

--- day 13/js.js
+++ day 13/js.js
@@ -31,10 +31,37 @@ console.assert(10 > 2 * 10, 'Erro' );
 // lvl 2 - 2.
 // Write a warning message using console.warn().
 
-console.warn('Alerta, vou ser um desenvolvedor profissional em breve')
+console.warn('Alerta, vou ser um desenvolvedor profissional em breve');
 
 // lvl 2 - 3.
 // Write an error message using console.error().
 
 console.error('Mind set poderoso para estudar com foco e objetivo');
 
+// lvl 3
+// Check the speed difference among the following loops: while, for, for of, forEach
+const names = ['Diego', 'Cristina', 'David', 'Ellen', "Joi"];
+const v = 0;
+// while for
+
+console.time('Regular while');
+while ( v < 6) {
+  v++;
+  console.log(v);
+}
+console.timeEnd('Regular while');
+
+// for of
+
+console.time('For of loop')
+  for ([namex] of names) {
+    console.log(names);
+
+  }
+console.timeEnd('For of lopp')
+
+// forEach
+console.time('ForEach loop')
+names.forEach(([userxx]) => {
+  console.log(names)
+}) 
