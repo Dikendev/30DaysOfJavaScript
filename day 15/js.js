c778d80491bbb3d778e6cf6a080830a4f47a87ed
diken.dev <diken.dev@gmail.com>
2022-08-16 23:30:24 -0300

    day 15 memories doc

--- day 15/js.js
+++ day 15/js.js
@@ -1,19 +1,47 @@
 class Person {
   constructor(firstName, lastName, age, country, city) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.country = country
-    this.city = city
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.country = country;
+    this.city = city;
+    this.score = 0
+    this.skills = []
   }
   getFullName() {
-    const fullName = this.firstName + ' ' + this.lastName
-    return fullName
+    const fullName = this.firstName + ' ' + this.lastName;
+    return fullName;
   }
-}
+  get getScore() {
+    return this.score;
+  }
+  get getSkills() {
+    return this.skills;
+  }
+  set setScore(score) {
+    this.score += score;
+  }
+  set setSkill(skill) {
+    this.skills.push(skill)
+  }
+};
 
-const Person1 = new Person('diego', 'kennedy', 250, 'brazil', 'blumenau')
-const Person2 = new Person('cris', 'freitas', 250, 'brazil', 'blumenau')
 
-console.log(Person1.getFullName())
-console.log(Person2.getFullName()) 
+const person1 = new Person('Asdoegabeneh', 'Yetayeh', 250, 'Finland', 'Helsinki')
+const person2 = new Person('Lidiya', 'Tekle', 28, 'Finland', 'Espoo')
+
+person1.setScore = 1;
+person1.setSkill = 'HTML';
+person1.setSkill = 'CSS';
+person1.setSkill = 'JavaScript';
+
+person2.setScore = 1;
+person2.setSkill = "Planning";
+person2.setSkill = 'Managing';
+person2.setSkill = 'Organiizing';
+
+console.log(person1.score)
+console.log(person2.score)
+
+console.log(person1.skills);
+console.log(person2.skills);
