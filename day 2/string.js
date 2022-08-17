8bfba2f22747b6751fcac26222b7af954e36e041
diken.dev <diken.dev@gmail.com>
2022-08-16 23:36:12 -0300

    fixed some coments

--- day 2/string.js
+++ day 2/string.js
@@ -1,85 +1,84 @@
-//let js = "Javascript é incrivel e irei terminar esse mês os conceitos basicos";
-//console.log(js.length);
+let js = "Javascript é incrivel e irei terminar esse mês os conceitos basicos";
+console.log(js.length);
 
-//let firstName = 'Asabeneh';
-//console.log(firstName.length);
-
-
-//let string = 'javaScript'
-//console.log(string.substring(4, 0));
+let firstName = 'Asabeneh';
+console.log(firstName.length);
 
 
-//console.log(firstName.toLowerCase());
-
-//console.log(js.toLowerCase());
-
-//console.log(js.split());
-//console.log(firstName.split());
-//console.log(firstName.split(""));
-
-//let countries = "Finland, Sweden, Norway, Denmark, and Iceland";
-
-//console.log(countries.split(","));
-//console.log(countries.split(", "));
+let string = 'javaScript'
+console.log(string.substring(4, 0));
 
 
-//console.log(challenger);
-//console.log(challenger.length);
-//console.log(challenger.toUpperCase());
-//console.log(challenger.toLowerCase());
-//console.log(challenger.substring(0,3));
-//console.log(challenger.replace(30, " "));
-//console.log(challenger.includes("30"));
-//console.log(challenger.split());
-//console.log(challenger.split(" "));
+console.log(firstName.toLowerCase());
 
-//let FGM = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
-//console.log(FGM.split(","));
+console.log(js.toLowerCase());
+
+console.log(js.split());
+console.log(firstName.split());
+console.log(firstName.split(""));
+
+let countries = "Finland, Sweden, Norway, Denmark, and Iceland";
+
+console.log(countries.split(","));
+console.log(countries.split(", "));
 
 
-//console.log(challenger.replace("JavaScript", "Python"));
-//console.log(challenger.charAt(15));
-//console.log(challenger.charCodeAt("J"));
-//console.log(challenger.indexOf("a"));
-//console.log(challenger.lastIndexOf("a"));
-//let phrase = "You can not end a sentence with because because because is a conjuntion";
-//console.log(phrase.indexOf("because"));
-//console.log(phrase.lastIndexOf("because"));
-//console.log(phrase.search("because"));
-//let challenger = "30 Days of JavaScript";
-//console.log(challenger.trim());
+console.log(challenger);
+console.log(challenger.length);
+console.log(challenger.toUpperCase());
+console.log(challenger.toLowerCase());
+console.log(challenger.substring(0,3));
+console.log(challenger.replace(30, " "));
+console.log(challenger.includes("30"));
+console.log(challenger.split());
+console.log(challenger.split(" "));
 
-//console.log(challenger.startsWith("3"))
-//console.log(challenger.endsWith("S  "))
-//let repeat = /a/gi
-//console.log(challenger.match(repeat));
-//let first = "30 Days of"
-//let second = "JavaScript"
-//let space = " "
-//console.log(first + space + second);
-//let challenger = "30 Days of JavaScript "
-//console.log(challenger.repeat(2))
+let FGM = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+console.log(FGM.split(","));
 
-//level 2
 
-//let quote = "There is no exercise better for the heart than reaching down and lifting people up.";
-//let author = "John Holmes teaches us to help one another.";
-//let space = " ";
+console.log(challenger.replace("JavaScript", "Python"));
+console.log(challenger.charAt(15));
+console.log(challenger.charCodeAt("J"));
+console.log(challenger.indexOf("a"));
+console.log(challenger.lastIndexOf("a"));
+let phrase = "You can not end a sentence with because because because is a conjuntion";
+console.log(phrase.indexOf("because"));
+console.log(phrase.lastIndexOf("because"));
+console.log(phrase.search("because"));
+let challenger = "30 Days of JavaScript";
+console.log(challenger.trim());
 
-//console.log(quote + space + author);
-//let numberS = "10"
-//console.log(parseInt(numberS));
+console.log(challenger.startsWith("3"))
+console.log(challenger.endsWith("S  "))
+let repeat = /a/gi
+console.log(challenger.match(repeat));
+let first = "30 Days of"
+// let second = "JavaScript"
+let space = " "
+console.log(first + space + second);
+let challenger = "30 Days of JavaScript "
+console.log(challenger.repeat(2))
 
-//console.log
-//let string = "Python and jargon"
-//let on = /on/gi
-//console.log(string.match(on))
+// level 2
 
-//let string = "I hope this course is no full of jargon"
-//let jargon = /jargon/gi
-//console.log(string.match(jargon));
+let quote = "There is no exercise better for the heart than reaching down and lifting people up.";
+let author = "John Holmes teaches us to help one another.";
+let space = " ";
 
-// const num = Math.floor(Math.random () * 100) // creates random number between 0 and 10
+console.log(quote + space + author);
+let numberS = "10"
+console.log(parseInt(numberS));
+
+console.log
+let string = "Python and jargon"
+let on = /on/gi
+console.log(string.match(on))
+
+let string = "I hope this course is no full of jargon"
+let jargon = /jargon/gi
+console.log(string.match(jargon));
+const num = Math.floor(Math.random () * 100) // creates random number between 0 and 10
 // console.log(num)
 
 
