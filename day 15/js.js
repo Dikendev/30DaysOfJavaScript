361d856594b5928ffcad8c6b9b510e8ded40431f
diken.dev <diken.dev@gmail.com>
2022-08-17 23:34:39 -0300

    added exercise lvl 1 - 2 to day 15

--- day 15/js.js
+++ day 15/js.js
@@ -1,4 +1,4 @@
-//lvl 1 - 1
+//lvl 1 - 1.
 // Create an Animal class. The class will have name, age, color, legs properties and create different methods
 
 class Animal {
@@ -14,8 +14,21 @@ class Animal {
   }
 }
 
-const Animal1 = new Animal('Joy', '6 months', 'white', 4);
-const Animal2 = new Animal('Capivara', '1 year', 'gray', 4);
+// lvl 1 - 2.
+// Create a Dog and Cat child class from the Animal Class.
 
+
+const Animal1 = new Animal('Joy', '6 months', 'white', 4);
+console.log(Animal1.getFullDesc());
 console.log(Animal1);
-console.log(Animal2);
+
+class DogAndCat extends Animal {
+  constructor(name, age, color, legs, gender) {
+    super(name, age, color, legs)
+    this.gender = gender;
+  }
+}
+
+const s1 = new DogAndCat('Capivara', '1 year', 'brown', 4, 'male');
+
+console.log(s1);
