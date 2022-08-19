a8eea6c12dd39d0c03c2ff6bf0d98d40bbb6fd04
diken.dev <diken.dev@gmail.com>
2022-08-18 23:16:19 -0300

    added studies of JSON

---
+++ day 16/js.js
@@ -0,0 +1,14 @@
+
+const user = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  country: 'Finland',
+  city: 'Helsinki',
+  email: 'alex@alex.com',
+  skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Python'],
+  age: 250,
+  isLoggedIn: false,
+  points: 30
+}
+
+const txt = JSON.stringify(user,['firstName'])
