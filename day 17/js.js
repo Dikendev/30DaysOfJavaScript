36b55237d2ebac2ce612316668b2f62232de744b
diken.dev <diken.dev@gmail.com>
2022-08-25 23:27:43 -0300

    added exercise lvl 2 to day 17

--- day 17/js.js
+++ day 17/js.js
@@ -6,4 +6,19 @@ localStorage.setItem('Age', 270);
 localStorage.setItem('Country', 'Brazil');
 localStorage.setItem('City', 'Santa Catarina');
 
+
+// lvl 2
+// Create a student object. The student object will have first name, last name, age, skills, country, enrolled keys and values for the keys. Store the student object in your browser localStorage.
+
+const student = {
+  firstName: 'Diego',
+  lastName: 'de Souza',
+  age: 270,
+  skills: ['HTML', 'CSS', 'JS'],
+  country: 'Brazil'
+}
+
+const studentJSON = JSON.stringify(student, undefined, 4);
+localStorage.setItem('student', studentJSON);
+
 console.log(localStorage);
