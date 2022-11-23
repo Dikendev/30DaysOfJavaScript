dcf64fb5f163acf13cfa39290179f1d4841d93a2
diken.dev <diken.dev@gmail.com>
2022-11-23 02:02:33 -0300

    day 21 start

---
+++ day 21/js.js
@@ -0,0 +1,8 @@
+const allTitles = document.getElementsByClassName('title');
+
+console.log(allTitles);
+console.log(allTitles.length);
+
+for( i = 0; i < allTitles.length; i++) {
+    console.log(allTitles[i]);
+}
